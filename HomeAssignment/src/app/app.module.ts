4636644afe42e159c55522c93dd749f2c3e6a1a0
tzion <tzion800@gmail.com>
2022-06-02 22:14:22 +0300

    create basic structure

--- HomeAssignment/src/app/app.module.ts
+++ HomeAssignment/src/app/app.module.ts
@@ -4,10 +4,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { SharedComponent } from './components/shared/shared.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SharedComponent
   ],
   imports: [
     BrowserModule,
